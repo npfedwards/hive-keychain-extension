fd528893a5809464ca241aebd9ae0c5328a4c51b
Saturno Mangieri <saturnob612@gmail.com>
2022-05-27 07:55:30 -0400

    Work in progress

--- src/__tests__/popup/pages/add-account/add-account-main.component.test.tsx
+++ src/__tests__/popup/pages/add-account/add-account-main.component.test.tsx
@@ -10,10 +10,16 @@ afterAll(() => {
   jest.clearAllMocks();
 });
 
-//TODO creates an overrided method for render to have
+//TODO creates an overrided method for render to have (OPtional)
 //<Provider store={store}>
 //{children}
 //</Provider>
+
+//TODO:
+//mock the store, using the initial values from store.
+//when mocking you'll be able to modify the state as needed
+//i.e in this case: load state.accounts so you can see workin the condition (accounts.length > 0) that will load another button.
+//this way we can control the E2E tests.
 
 //todo also a handler for functions as chrome.i18n.getMessage
 chrome.i18n.getMessage = jest
@@ -22,6 +28,7 @@ chrome.i18n.getMessage = jest
 
 describe('add-account-main.component tests:\n', () => {
   test('should first', () => {
+    console.log(store.getState());
     const addAccountComponent = renderer.create(
       <Provider store={store}>
         <AddAccountMainComponent />
@@ -29,7 +36,18 @@ describe('add-account-main.component tests:\n', () => {
     );
     // const addAccountTree = addAccountComponent.toJSON();
     // console.log(addAccountTree);
-    const addComponentRoot = addAccountComponent.root;
-    console.log(addComponentRoot.findByProps({ navigateTo: {} }));
+    // const addComponentRoot = addAccountComponent.root;
+    // addComponentRoot.children.forEach((children) => {
+    //   console.log(children);
+    // });
+    //console.log(addComponentRoot.findByProps({ navigateTo: {} }));
+    renderer.act(() => {
+      const buttons = addAccountComponent.root.findAllByType('button');
+      buttons[0].props.onClick();
+      buttons.forEach((button) => {
+        console.log(button.props);
+      });
+    });
+    console.log(store.getState().navigation);
   });
 });
